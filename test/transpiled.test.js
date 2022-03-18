5cf523018fa59cc5c40edce65cb998ee807bb66f
Andres Correa Casablanca <andres.casablanca@nearform.com>
2022-03-18 12:21:05 +0100

    test: cover ts -> esm generation

--- test/transpiled.test.js
+++ test/transpiled.test.js
@@ -10,7 +10,41 @@ test('transpiled-ts-to-es5', function (t) {
 
   const dest = file()
   const stream = new ThreadStream({
-    filename: join(__dirname, 'ts', 'to-file.es5.js'),
+    filename: join(__dirname, 'ts', 'to-file.es5.cjs'),
+    workerData: { dest },
+    sync: true
+  })
+
+  // There are arbitrary checks, the important aspect of this test is to ensure
+  // that we can properly load the transpiled file into our worker thread.
+  t.same(stream.writableEnded, false)
+  stream.end()
+  t.same(stream.writableEnded, true)
+})
+
+test('transpiled-ts-to-es6', function (t) {
+  t.plan(2)
+
+  const dest = file()
+  const stream = new ThreadStream({
+    filename: join(__dirname, 'ts', 'to-file.es6.cjs'),
+    workerData: { dest },
+    sync: true
+  })
+
+  // There are arbitrary checks, the important aspect of this test is to ensure
+  // that we can properly load the transpiled file into our worker thread.
+  t.same(stream.writableEnded, false)
+  stream.end()
+  t.same(stream.writableEnded, true)
+})
+
+test('transpiled-ts-to-es2017', function (t) {
+  t.plan(2)
+
+  const dest = file()
+  const stream = new ThreadStream({
+    filename: join(__dirname, 'ts', 'to-file.es2017.cjs'),
     workerData: { dest },
     sync: true
   })
