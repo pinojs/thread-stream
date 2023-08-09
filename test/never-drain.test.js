c16fc93a98aa1d432baeb3351092ed32e2ae5bd6
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-08-09 21:46:24 +0300

    emit warning when worker gracefully exit before finish

---
+++ test/never-drain.test.js
@@ -0,0 +1,40 @@
+const { test } = require('tap')
+const ThreadStream = require('../index')
+const { join } = require('path')
+
+function sleep (ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+test('emit warning when the worker gracefully exit without the stream ended', async function (t) {
+  const expectedWarning = 'ThreadStream: process exited before destination stream was drained. this may indicate that the destination stream try to write to a another missing stream'
+  const stream = new ThreadStream({
+    filename: join(__dirname, 'to-next.js')
+  })
+
+  let streamWarning
+  function saveWarning (e) {
+    if (e.message === expectedWarning) {
+      streamWarning = e
+    }
+  }
+  process.on('warning', saveWarning)
+
+  for (let i = 0; i < 10_000; i++) {
+    if (streamWarning?.message === expectedWarning) {
+      break
+    }
+    stream.write('hello')
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1)
+    })
+  }
+
+  process.off('warning', saveWarning)
+  await sleep(10)
+
+  t.equal(stream.destroyed, true)
+  t.equal(streamWarning?.message, expectedWarning)
+})
