de08119686d1e9ab6453e2f5e9ba09a60abeac21
kyle <kylejeske@users.noreply.github.com>
2024-05-31 07:34:08 -0700

    Set type of transferList to workerThreads.TransferListItem (#152)
    * fix: type reference to DOM lib, Transferable
    
    * fix: replace type 'Transferable' with 'TransferListItem'

--- index.d.ts
+++ index.d.ts
@@ -86,7 +86,7 @@ declare class ThreadStream extends EventEmitter {
    * @param transferList an optional list of transferable objects to be transferred to the Worker context.
    * @returns {boolean} true if the event had listeners, false otherwise.
    */
-  emit(eventName: 'message', message: any, transferList?: Transferable[]): boolean
+  emit(eventName: 'message', message: any, transferList?: workerThreads.TransferListItem[]): boolean
 }
 
 export = ThreadStream;
