48ecd888750c0e8e3bf7bb50bfd267596fd6530d
Matteo Collina <hello@matteocollina.com>
2026-09-02 10:08:20 +0200

    fix: retain ending streams until worker exit

--- test/end.test.js
+++ test/end.test.js
@@ -31,6 +31,41 @@ test('destroy support', function (t, done) {
   stream.end()
 })
 
+test('end keeps the stream alive until close', { skip: typeof global.gc !== 'function' }, function (t, done) {
+  const dest = file()
+
+  const onClose = () => {
+    clearInterval(gcTimer)
+    clearTimeout(timeout)
+    readFile(dest, 'utf8', (err, data) => {
+      assert.ifError(err)
+      assert.strictEqual(data, 'hello world\nsomething else\n')
+      done()
+    })
+  }
+
+  function setup () {
+    const stream = new ThreadStream({
+      filename: join(__dirname, 'to-file-on-final.js'),
+      workerData: { dest },
+      sync: true
+    })
+
+    stream.on('close', onClose)
+    assert.ok(stream.write('hello world\n'))
+    assert.ok(stream.write('something else\n'))
+    stream.end()
+  }
+
+  setup()
+
+  const gcTimer = setInterval(global.gc, 1)
+  const timeout = setTimeout(() => {
+    clearInterval(gcTimer)
+    assert.fail('stream was collected before close')
+  }, 5000)
+})
+
 test('synchronous _final support', function (t, done) {
   const dest = file()
   const stream = new ThreadStream({
